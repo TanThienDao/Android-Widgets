0476e031f568ae9275645c2a505a1dd5d64f7b9e
Tan Dao <ttdao3@asu.edu>
2024-10-05 23:39:03 -0700

    first commit checkbox pizza

---
+++ app/src/main/java/com/example/checkbox/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.checkbox;
+
+import android.os.Bundle;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    CheckBox checkBox;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        checkBox = findViewById(R.id.checkbox);
+        checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    // Code to execute when checkbox id checked
+                    Toast.makeText(MainActivity.this,
+                            "The Checkbox is checked",
+                            Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this,
+                            "The Check is not checked",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
