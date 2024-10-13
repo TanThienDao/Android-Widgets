ece7d21744fce849d5e0e9ee6323ae90cca83bbb
Tan Dao <ttdao3@asu.edu>
2024-10-12 17:46:26 -0700

    add spinner

--- app/src/main/java/com/example/checkbox/MainActivity.java
+++ app/src/main/java/com/example/checkbox/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.checkbox;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -13,6 +17,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
     CheckBox checkBox;
+
+    RadioGroup radioGroup;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,5 +48,36 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        radioGroup = findViewById(R.id.radioGroup);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                RadioButton radioButton = findViewById(checkedId);
+                Toast.makeText(MainActivity.this,
+                        "You selected: " +radioButton.getText(),
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        spinner = findViewById(R.id.spinner);
+
+        // Data Source:
+        String[] courses = {"C/C++", "Java", "Kotlin","Python"};
+
+        // Array Adapter : used to populate the  'Spinner' with
+        //                 items from a string array resource
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                        this,
+                android.R.layout.simple_spinner_item,
+                courses
+                );
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
+
+
+
+
     }
 }
